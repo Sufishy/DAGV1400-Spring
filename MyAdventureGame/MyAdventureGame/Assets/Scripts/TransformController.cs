fdda73ec3ac54ca92e326edc3595186d3850c859
Sufishy <suvischramm.school@gmail.com>
2025-02-02 23:12:46 -0700

    Diagonal Transform
    changed the transform script to move the objects from up and down to a diagonal going northwest
    Fixed tilt shift, stopped using rotation, and deleted "ground" blocks from view.

--- MyAdventureGame/MyAdventureGame/Assets/Scripts/TransformController.cs
+++ MyAdventureGame/MyAdventureGame/Assets/Scripts/TransformController.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 
-public class TranformController : MonoBehaviour
+public class TransformController : MonoBehaviour
 {
     private void Update()
     {
-        // Move the target GameObject
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        // Move the target Diagonally by adding y
+        var x = Mathf.PingPong(Time.time, 2);
+       var y = Mathf.PingPong(Time.time, 2);
+        
+        // this - y makes the characters move middle to left
+        var p = new Vector3(-y, x, 1);
         transform.position = p;
         
         // Rotate the target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime);
     }
 }
