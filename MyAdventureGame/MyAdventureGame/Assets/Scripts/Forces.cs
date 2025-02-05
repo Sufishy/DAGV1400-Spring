31b94bd1a2713d5f18ca54e164f9e5c32b3b4f28
Sufishy <suvischramm.school@gmail.com>
2025-02-05 13:02:06 -0700

    Added Collision Recognition
    Added the collision detection to the forces script

--- MyAdventureGame/MyAdventureGame/Assets/Scripts/Forces.cs
+++ MyAdventureGame/MyAdventureGame/Assets/Scripts/Forces.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Forces : MonoBehaviour
 {
     private Rigidbody rb;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    // This is the collision line
+    void OnCollisionEnter(Collision collision)
+    {
+        // next collision script
+        Debug.Log("Collision detected with " + collision.gameObject.name);
+        
         rb.AddForce(Vector3.right * 500);
     }
 }
