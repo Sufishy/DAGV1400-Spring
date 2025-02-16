f50ff264066464c31dd333469dec2f5e425cff86
Sufishy <suvischramm.school@gmail.com>
2025-02-15 21:28:48 -0700

    Wrote Characteranimationcontroller script
    had to alter the code slightly, but mostly copied the code from the script provided by professor

---
+++ MyAdventureGame/MyAdventureGame/Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        //Triggers the Double Jump Animation
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("Jump");
+        }
+        else
+        {
+            animator.SetTrigger("Idle");
+        }
+
+        //Triggers the Hit Animation
+        if (Input.GetKeyDown(KeyCode.H)) // Change from GetButtonDown to GetKeyDown
+        {
+            animator.SetTrigger("Hit");
+        }
+        else
+        {
+            animator.SetTrigger("Idle");
+        }
+        //Triggers the Hit Animation
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Fall");
+        }
+        else
+        {
+            animator.SetTrigger("Idle");
+        }
+    }
+}
