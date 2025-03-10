9a4fcd23ab8d3d5c74b8deec3430fcee5def53cb
Sufishy <suvischramm.school@gmail.com>
2025-03-09 18:01:50 -0600

    Finally got the saw to shrink on collision
    Got the saw to shrink on collision. Made a script for it that was useless. Altered the simple trigger behavior

--- MyAdventureGame/MyAdventureGame/Assets/Scripts/SimpleTriggerEventBehaviour.cs
+++ MyAdventureGame/MyAdventureGame/Assets/Scripts/SimpleTriggerEventBehaviour.cs
@@ -7,13 +7,17 @@ using UnityEngine.UI;
 public class SimpleTriggerEventBehaviour : MonoBehaviour
 {
     public UnityEvent triggerEvent;
-    public int damageAmount = 10; //How much damage is dealt
+    public int damageAmount = 10; // How much damage is dealt
     public int scoreIncreaseAmount = 1;
+    public Vector3 shrinkScale = new Vector3(0.5f, 0.5f, 0.5f); // The scale to shrink to
 
     private void OnTriggerEnter(Collider other)
     {
-        // this triggers the event
+        // This triggers the event
         triggerEvent.Invoke();
+
+        // Shrink the saw on collision
+        transform.localScale = shrinkScale;
 
         Animator animator = other.GetComponent<Animator>();
 
@@ -22,7 +26,7 @@ public class SimpleTriggerEventBehaviour : MonoBehaviour
             animator.SetTrigger("Hit"); // Trigger hit animation
         }
 
-        //Apply damage
+        // Apply damage
         PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
         if (playerHealth != null)
         {
