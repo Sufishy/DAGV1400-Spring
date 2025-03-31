2adbde3f4225f572341955436b8303ec24c2951e
Sufishy <suvischramm.school@gmail.com>
2025-03-31 16:36:02 -0600

    Created Scripts
    Created id scripts for the watermelon, and applied them to the watermelon. Created fire sprite and applied a collider to it.

---
+++ MyAdventureGame/MyAdventureGame/Assets/Scripts/Scene 11/SimpleIDMatchBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleIDMatchBehaviour : MonoBehaviour
+{
+    public Id id;
+    public UnityEvent matchEvent, noMatchEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherID = other.GetComponent<SimpleIDBehaviour>();
+
+        if (otherID.id == id)
+        {
+            matchEvent.Invoke();
+            Debug.Log("Matched ID: " + id);
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+            Debug.Log("No Match: " + id);
+        }
+    }
+}
